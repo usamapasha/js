4316930bda3f958f482fc0a9611ba25ffa593903
usama <pashausamapasha@gmail.com>
2024-01-09 09:35:41 -0800

    work done

--- pgs/2.js
+++ pgs/2.js
@@ -12,3 +12,23 @@ console.log(typeof(firstname));
 console.log(typeof(age));
 console.table(firstname , age , x , y , iscnic);
 
+// TWO TYPE OF DATA TYPES
+// PRIMITIVE STRING , INT , NUMBER , BOOLEAN ETC
+// NON PRIMITIVE ARRAY , OBJECT , FUNCTION , ETC
+
+// THREE TYPE OF DECLERATION
+
+// VAR CAN BE REDECLARE AND UPDATED
+// LET CAN`T BE REDECLARED BUT IT CAN BE UPPDATED
+// CONST CANT` BE REDECLARED OR CANT BE UPDATED
+
+
+var city = "karachi";
+console.log(city);
+ city = "islamabad";
+ console.log(city)
+ var city = "lahore"
+console.log(city);
+
+let myage = 17;
+console.log(myage);
