79d6e3c1ee040f582bdf76e22526cf600aeb335e
usama <pashausamapasha@gmail.com>
2024-01-09 08:43:45 -0800

    done

---
+++ pgs/2.js
@@ -0,0 +1,14 @@
+console.log ("welcome to pjs");
+console.log ("hellow world");
+
+firstname = "usama";
+age = "17";
+x = null;
+y = undefined;
+iscnic = true;
+
+console.log(typeof(iscnic));
+console.log(typeof(firstname));
+console.log(typeof(age));
+console.table(firstname , age , x , y , iscnic);
+
